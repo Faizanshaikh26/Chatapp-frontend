887932aa16b68061438dad0f63ee7a07c1fd84b4
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-07-13 15:48:47 +0530

    d

--- src/Socket.jsx
+++ src/Socket.jsx
@@ -7,8 +7,11 @@ const SocketContext = createContext();
 
 const SocketProvider = ({ children }) => {
   const socket = useMemo(() => {
-    return io(server, {
+    return io('wss://chatapp-api-ursy.onrender.com', {
       withCredentials: true,
+      path: '/socket.io',
+  transports: ['websocket','pooling'],
+  secure: true,
     });
   }, []);
   
